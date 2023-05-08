30bca3e5e19b52c7da12ceccc4daea25553fba7c
je-el <maldojeweln@gmail.com>
2023-05-08 14:45:41 -0800

    missing }'s

--- kernelshellProjectTest.c
+++ kernelshellProjectTest.c
@@ -8,27 +8,26 @@
 #define MAX_ARGS 64
 #define MAX_CMD_LEN 1024
 
-void display_kernel_atlaunch() { //something cool at launch
-    	printf("      __________________\n");
-    	printf("    .-'  \\ _.-''-._ /  '-.\n");
-    	printf("  .-\\/   .'.      .'.   /\\-. \n");
-    	printf(" _'/  \\.\"   \'.  .\'   \"/./  \\_\n");
-    	printf(":======:======::======:======:====:\n");
-    	printf(" '. \".  \\     ''     /  .\"  .'\n");
-    	printf("   '. \". \\   :  :   / .\" .'\n");
-    	printf("     '.'. \\ '  ' /  .'  .'\n");
-    	printf("       ':\\  \\:/. :/   :'\n");
-    	printf("         '.\\     /.  .'\n");
-    	printf("           '\\   /. '\n");
-	printf("You can execute external commands, use `help` to see the different internal commands you can use or type `exit` to exit the shell\n");
-}
+
 
 int main() {
-
+    printf("      __________________\n");
+    printf("    .-'  \\ _.-''-._ /  '-.\n");
+    printf("  .-\\/   .'.      .'.   /\\-. \n");
+    printf(" _'/  \\.\"   \'.  .\'   \"/./  \\_\n");
+    printf(":======:======::======:======:===:\n");
+    printf(" '. \".  \\     ''     /  .\" .'\n");
+    printf("   '. \". \\   :  :   / .\" .'\n");
+    printf("     '.'. \\ '  ' /  .'  .'\n");
+    printf("       ':\\  \\:/. :/   :'\n");
+    printf("         '.\\     /. .'\n");
+    printf("           '\\   /.'\n");
+    printf("You can execute external commands, use `help` to see the different internal commands you can use or type `exit` to exit the shell\n");
+    printf("\nThis Kernel Shell was created by Lisa Jacklin and Jewel Maldonado");
     	char input[MAX_CMD_LEN];
     	char *args[MAX_ARGS];
     	int status;
-	display_kernal_atlaunch();
+    
     	while (1) { //while loop start
         	printf("$ ");
         	fflush(stdout);
@@ -36,7 +35,7 @@ int main() {
         	// Read command input from user
         	if (!fgets(input, MAX_CMD_LEN, stdin)){
         	    break;
-		}
+            }
         	// Parse command into arguments
         	int i = 0;
         	args[i] = strtok(input, " \t\n");
@@ -53,45 +52,44 @@ int main() {
 	            // Handle internal commands
 	            if (args[0] && strcmp(args[0], "exit") == 0){
 	                break;
-		    } else if (args[0] && strcmp(args[0], "print") == 0){
+                } else if (args[0] && strcmp(args[0], "print") == 0){
 	                printf("Current pid: %d\n", getpid());
-		    } else if (args[0] && strcmp(args[0], "help") == 0) {
+                } else if (args[0] && strcmp(args[0], "help") == 0) {
 	                printf("This is a simple shell that can execute external commands as well as internal commands.\n");
 	                printf("To execute an external command, simply enter the name of the command and its arguments.\n");
 	                printf("To execute an internal command, enter one of the following:\n");
 	                printf("\texit: terminate the shell\n");
 	                printf("\tprint: print out the current pid\n");
 	                printf("\thelp: display this help message\n");
-		    } else {
-	                // Handle external commands
+                } else {
+                    // Handle external commands
 	                pid_t pid = fork();
 	                if (pid == -1) {
 	                    perror("fork");
 	                    exit(EXIT_FAILURE);
 	                }
 	                else if (pid == 0) {
-				// Child process
-				execvp(args[0], args);
-				perror(args[0]);
-				exit(EXIT_FAILURE);
+                        // Child process
+                        execvp(args[0], args);
+                        perror(args[0]);
+                        exit(EXIT_FAILURE);
         	        }
         	    }
-        	}
-        	else {
+        	} else {
         	    // Single command
         	    // Handle internal commands
-        	    	if (args[0] && strcmp(args[0], "exit") == 0){ 
-        	        	break;
-			} else if (args[0] && strcmp(args[0], "print") == 0)
+                if (args[0] && strcmp(args[0], "exit") == 0){
+                    break;
+                } else if (args[0] && strcmp(args[0], "print") == 0){
                 		printf("Current pid: %d\n", getpid());
-			} else if (args[0] && strcmp(args[0], "help") == 0) {
+                } else if (args[0] && strcmp(args[0], "help") == 0) {
                 		printf("This is a simple shell that can execute external commands as well as internal commands.\n");
                 		printf("To execute an external command, simply enter the name of the command and its arguments.\n");
                 		printf("To execute an internal command, enter one of the following:\n");
                 		printf("\texit: terminate the shell\n");
                 		printf("\tprint: print out the current pid\n");
                 		printf("\thelp: display this help message\n");
-			} else {
+                } else {
                 		// Handle external commands
                 		pid_t pid = fork();
                 		if (pid == -1) {
@@ -101,14 +99,13 @@ int main() {
                 		else if (pid == 0) {
                			    // Child process
                 		    execvp(args[0], args);
-                    		    perror(args[0]);
-                    	            exit(EXIT_FAILURE);
-                		}
-                		else {
+                            perror(args[0]);
+                            exit(EXIT_FAILURE);
+                		} else {
                     			// Parent process
                     			waitpid(pid, &status, 0);
                 		}		
-            		}
+                }
         	}
     	} //while loop end
 
